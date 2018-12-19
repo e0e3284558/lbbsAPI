a2c62340f35eb88e196e2eb9e39b2624d5ef25b3
vagrant <vagrant@homestead>
2018-12-19 07:37:55 +0000

    安装adminisrrator后台

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -57,6 +57,13 @@
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
+                            @can('manage_contents')
+                                <li><a href="{{url('administrator.uri')}}">
+                                        <span class="glyphicon glyphicon-dashboard" aria-hidden="true"></span>
+                                        管理后台
+                                    </a>
+                                </li>
+                            @endcan
                             <li><a href="{{route('users.show',Auth::id())}}">
                                     <span class="glyphicon glyphicon-user"></span>
                                     个人中心
