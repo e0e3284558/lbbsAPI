c21d713fb3dbe6a821f320a12394132e2cca66c9
vagrant <vagrant@homestead>
2018-12-19 06:55:49 +0000

    安装laravel-permission权限包

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
@@ -33,14 +34,21 @@ class UsersTableSeeder extends Seeder
         $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
 
         // 插入到数据库中
-        \App\Models\User::insert($user_array);
+        User::insert($user_array);
 
         // 单独处理第一个用户的数据
-        $user = \App\Models\User::find(1);
+        $user = User::find(1);
         $user->name = 'BF';
         $user->email = 'bifei0827@vip.qq.com';
         $user->avatar = 'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200';
         $user->save();
+
+        // 初始化用户角色，将 1 号用户指派为『站长』
+        $user->assignRole('Founder');
+
+        // 将 2 号用户指派为『管理员』
+        $user = User::find(2);
+        $user->assignRole('Maintainer');
 
     }
 }
