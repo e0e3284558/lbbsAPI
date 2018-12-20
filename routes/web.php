e1bb0bf3b75f11276ba637b99d923ac6f9745855
vagrant <vagrant@homestead>
2018-12-20 04:34:56 +0000

    管理员权限

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,6 @@ Route::post('upload_image','TopicsController@uploadImage')->name('topics.upload_
 Route::resource('notifications','NotificationsController',['only'=>['index']]);
 
 Route::resource('replies', 'RepliesController', ['only' => [ 'store', 'destroy']]);
+
+
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
