5ec8d660fa3d3396e895bbc6c5aec087b5bbd6da
Your Name <you@example.com>
2019-01-07 05:05:04 +0000

    更新用户信息

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Requests\Api\UserRequest;
+use App\Models\Image;
 use App\Models\User;
 use App\Transformers\UserTransformer;
 use Illuminate\Http\Request;
@@ -13,37 +14,52 @@ class UsersController extends Controller
 {
     public function store(UserRequest $request)
     {
-        $verifyData=Cache::get($request->verification_key);
+        $verifyData = Cache::get($request->verification_key);
 
-        if (!$verifyData){
-            return $this->response->error('验证码已失效',422);
+        if (!$verifyData) {
+            return $this->response->error('验证码已失效', 422);
         }
 
-        if (!hash_equals($verifyData['code'],$request->verification_code)){
+        if (!hash_equals($verifyData['code'], $request->verification_code)) {
             // 返回401
             return $this->response->errorUnauthorized('验证码错误');
         }
 
-        $user=User::create([
-            'name'=>$request->name,
-            'phone'=>$verifyData['phone'],
-            'password'=>bcrypt($request->password),
+        $user = User::create([
+            'name' => $request->name,
+            'phone' => $verifyData['phone'],
+            'password' => bcrypt($request->password),
         ]);
 
         // 清除验证码缓存
         Cache::forget($request->verification_key);
 
-        return $this->response->item($user,new UserTransformer())
+        return $this->response->item($user, new UserTransformer())
             ->setMeta([
-                'access_token'=>Auth::guard('api')->fromUser($user),
-                'token_type'=>'Bearer',
-                'expires_in'=>Auth::guard('api')->factory()->getTTL()*60
+                'access_token' => Auth::guard('api')->fromUser($user),
+                'token_type' => 'Bearer',
+                'expires_in' => Auth::guard('api')->factory()->getTTL() * 60
             ])
             ->setStatusCode(201);
     }
 
     public function me()
     {
-        return $this->response->item($this->user,new UserTransformer());
+        return $this->response->item($this->user, new UserTransformer());
+    }
+
+    public function update(UserRequest $request)
+    {
+        $user = $this->user();
+
+        $attributes = $request->only(['name', 'email', 'introduction']);
+
+        if ($request->avatar_image_id){
+            $image=Image::find($request->avatar_image_id);
+            $attributes['avatar']=$image->path;
+        }
+        $user->update($attributes);
+
+        return $this->response->item($user,new UserTransformer());
     }
 }
