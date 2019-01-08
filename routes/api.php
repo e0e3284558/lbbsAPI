54f31683fdd721387a3e8d6f7b9e894c3464c0ff
Your Name <you@example.com>
2019-01-08 11:55:37 +0800

    话题首页

--- routes/api.php
+++ routes/api.php
@@ -36,9 +36,13 @@ $api->version('v1', [
         // 删除token
         $api->delete('authorizations/current', 'AuthorizationsController@destroy')
             ->name('api.authorizations.destroy');
-
+        // 游客可以访问的接口
         $api->get('categories', 'CategoriesController@index')
             ->name('api.categories.index');
+        $api->get('topics', 'TopicsController@index')
+            ->name('api.topics.index');
+        $api->get('users/{user}/topics', 'TopicsController@userIndex')
+            ->name('api.users.topics.index');
 
         // 需要token验证的接口
         $api->group(['middleware' => 'api.auth'], function ($api) {
